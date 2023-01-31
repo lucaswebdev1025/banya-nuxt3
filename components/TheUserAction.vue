0ccf533cbf8c05658d9f243d004d26a8d07708c3
tonight11 <godofspeedman@gmail.com>
2023-01-31 15:25:58 +0300

    feat: product page. breadcrumbs&actions

---
+++ components/TheUserAction.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+
+<template>
+	<div class="user-action">
+		<img class="user-action__item" src="~/assets/img/icon/like.svg" alt="" />
+
+		<img class="user-action__item" src="~/assets/img/icon/dislike.svg" alt="" />
+
+		<img class="user-action__item" src="~/assets/img/icon/msg.svg" alt="" />
+
+		<img class="user-action__item" src="~/assets/img/icon/share.svg" alt="" />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	@import '~/assets/styles/vars.scss';
+	.user-action {
+		display: flex;
+		align-items: center;
+		background-color: $default-color;
+		border-radius: 28px;
+
+		padding: 8px;
+		&__item {
+			padding: 0 7px;
+			border-right: 1.25px solid #e1e5f2;
+			&:last-child {
+				border: none;
+			}
+		}
+	}
+</style>
