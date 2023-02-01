192f5165f1229d445bf84ab1806b6ba376106872
tonight11 <godofspeedman@gmail.com>
2023-02-01 18:28:27 +0300

    feat: review card

--- components/ProductCard/Main.vue
+++ components/ProductCard/Main.vue
@@ -1,42 +1,4 @@
-<script setup>
-	import img1 from '~/assets/img/product-card/service1.png';
-	import img2 from '~/assets/img/product-card/service2.png';
-	import img3 from '~/assets/img/product-card/service3.png';
-	const service = [
-		{
-			title: 'SPA-клуб на метро Крестьянкая застава',
-			price: false,
-			logo: false,
-			place: false,
-			desc: false,
-			img: false,
-		},
-		{
-			title: 'SPA-клуб на метро Крестьянкая застава',
-			price: 2300,
-			logo: false,
-			place: false,
-			desc: false,
-			img: img1,
-		},
-		{
-			title: 'SPA-клуб на метро Крестьянкая застава',
-			price: 4300,
-			logo: true,
-			place: '7 залов',
-			desc: 'Особое - спина-живот | Медовое - 2 парения «Особое» без перерыва | Королевское - 3 парения «Особ...',
-			img: img2,
-		},
-		{
-			title: 'SPA-клуб на метро Крестьянкая застава',
-			price: 6800,
-			logo: true,
-			place: '7 залов',
-			desc: 'Особое - спина-живот | Медовое - 2 парения «Особое» без перерыва | Королевское - 3 парения «Особ...',
-			img: img3,
-		},
-	];
-</script>
+<script setup></script>
 
 <template>
 	<div class="card">
@@ -64,29 +26,8 @@
 		</div>
 	</div>
 	<ProductCardBlocked v-if="true" />
-	<div class="card card-item">
-		<div class="card-item__left">
-			<div class="card-item__top">
-				<div class="card-item__title h2">Сауны и бани с массжем в Москве</div>
-				<div class="card-item__text h5">
-					Сауны бани с массажем - мы нашли для вас 550 саун <br />
-					и бань в городе Москва
-				</div>
-			</div>
-			<ProductCardService
-				v-for="item in service"
-				:title="item.title"
-				:desc="item.desc"
-				:logo="item.logo"
-				:price="item.price"
-				:place="item.place"
-				:img="item.img"
-			/>
-		</div>
-		<div class="card-item__right">
-			<!-- map -->
-		</div>
-	</div>
+	<ProductCardService />
+	<ProductCardReview />
 </template>
 
 <style lang="scss" scoped>
@@ -98,16 +39,6 @@
 			color: $secondary-color;
 			margin-bottom: 5.5rem;
 			position: relative;
-		}
-
-		&__top-tariff {
-			position: absolute;
-			top: 0.5rem;
-			left: -4.5rem;
-			background: #8f99ba;
-			color: white;
-			padding: 1rem;
-			border-radius: 6px 0 0px 0px;
 		}
 
 		&__top-left {
@@ -137,33 +68,6 @@
 
 		&__right {
 			width: 100%;
-		}
-	}
-
-	.card-item {
-		margin-top: 2rem;
-		display: flex;
-		gap: 2rem;
-		&__left {
-			width: 100%;
-		}
-
-		&__top {
-			padding-bottom: 1rem;
-			border-bottom: 1px solid #e1e5f2;
-		}
-
-		&__title {
-			color: $mainFontColor;
-			margin-bottom: 3px;
-		}
-
-		&__text {
-			color: $secondary-color;
-		}
-
-		&__right {
-			width: 65rem;
 		}
 	}
 </style>
