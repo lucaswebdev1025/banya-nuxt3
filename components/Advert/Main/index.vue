a9b1ee8e12c5c028ed797c0eb0d72a171b722649
Mazaev Andrey <mazaev2000@gmail.com>
2023-02-17 01:55:54 +0300

    feat: update advert

--- components/Advert/Main/index.vue
+++ components/Advert/Main/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="advert-main">
     <div class="advert-main__item">
-      <div class="advert-main__title advert-main__title_first">Новое объявление</div>
-      <div class="advert-main__subtitle">Объявление будет доступно на <a target="_blank" href="/">Баня.ру</a> и в нашем <a target="_blank" href="/">мобильном приложении</a></div>
+      <div class="advert-main__title advert-main__title_first">
+        Новое объявление
+      </div>
+      <div class="advert-main__subtitle">
+        Объявление будет доступно на <a target="_blank" href="/">Баня.ру</a> и в
+        нашем <a target="_blank" href="/">мобильном приложении</a>
+      </div>
       <AdvertMainType />
     </div>
 
@@ -18,7 +23,8 @@
       <AdvertMainContacts />
       <div class="advert-main__title advert-main__title_zal">Зал</div>
       <div class="advert-main__zal">
-        <span>Активность <img src="" alt="" /></span>
+        <span class="advert-main__zal-active">Активность</span>
+        <span class="info-icon">i</span>
         <input class="switch" type="checkbox" />
       </div>
       <AdvertMainZalName />
@@ -33,19 +39,32 @@
 </template>
 
 <style lang="scss" scoped>
+.info-icon {
+  width: 3.2rem;
+  height: 3.2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 1.3rem;
+  margin-right: 6rem;
+  background: #ffffff;
+  box-shadow: 1px 4px 12px rgba(166, 175, 205, 0.36);
+  border-radius: 100px;
+  color: $green-color;
+}
 .advert-main {
   display: flex;
   flex-direction: column;
   gap: 4.3rem;
   max-width: 106rem;
   width: 100%;
-  &__item{
+  &__item {
     display: flex;
-  flex-direction: column;
-  background: #ffffff;
-  box-shadow: 0px 11px 48px rgba(123, 129, 148, 0.15);
-  border-radius: 2.3rem;
-  padding: 0 5rem;
+    flex-direction: column;
+    background: #ffffff;
+    box-shadow: 0px 11px 48px rgba(123, 129, 148, 0.15);
+    border-radius: 2.3rem;
+    padding: 0 5rem;
   }
   &__title {
     text-align: center;
@@ -54,30 +73,35 @@
     line-height: 100%;
     margin-top: 3.9rem;
     margin-bottom: 5.7rem;
-    &_first{
+    &_first {
       margin-bottom: 1.5rem;
     }
     &_zal {
       margin-bottom: 1.9rem;
     }
+    &__zal-active {
+      font-weight: 700;
+      font-size: 2rem;
+      line-height: 130%;
+    }
   }
-  &__subtitle{
+  &__subtitle {
     text-align: center;
     margin-bottom: 2.6rem;
-    color:$secondary-color;
+    color: $secondary-color;
     max-width: 26rem;
     margin-left: auto;
     margin-right: auto;
-    & a{
-      color:$green-color;
+    & a {
+      color: $green-color;
     }
   }
   &__zal {
     justify-content: center;
     display: flex;
-    gap: 6rem;
-    padding-bottom: 5.9rem;
-    border-bottom: 1px solid rgba(143, 153, 186, 0.2);
+    align-items: center;
+    
+    padding-bottom: 6.9rem;
   }
 }
 </style>
